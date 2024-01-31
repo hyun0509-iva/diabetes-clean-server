3b06ebeabce7ea6a71049204ac3acd08eeaea17e
hyun0509-iva <skyblue5030@gmail.com>
2024-01-31 18:51:36 +0900

    chore: ts + class 패턴으로 마이그레이션

--- src/app.ts
+++ src/app.ts
@@ -1,56 +1,25 @@
-import express, { Application } from "express";
-import morgan from 'morgan';
+import express, { Application, Request, Response } from "express";
+import morgan from "morgan";
 import mongoose from "mongoose";
 import cors from "cors";
 import helmet from "helmet";
 import errorMiddleware from "./middleware/error.middleware";
 import { Controller } from "./interfaces/controller";
 import cookieParser from "cookie-parser";
-// import config from "./config/key.js";
-// import dbconnect from "./dbconfig.js";
-// import {
-//   usersRouter,
-//   authRouter,
-//   commentRouter,
-//   contentsRouter,
-//   diabetesRouter,
-//   likeRouter,
-//   searchRouter,
-//   imageRouter
-// } from "./routes/index.js";
-// import {
-//   BASIC_API_URL,
-//   COMMENT,
-//   LIKE,
-//   SEARCH,
-//   USERS,
-//   AUTH,
-//   DIABETES,
-//   CONTENTS,
-//   BASIC_CLIENT_URL
-// } from "./constants/path.js";
-// import path from "path";
-// import { fileURLToPath } from "url";
-// app.use("/img/uimg", express.static(path.join(__dirname, "uploads/userImg")));
-// app.use("/img/pimg", express.static(path.join(__dirname, "uploads/postImg")));
-
-// app.use(`${BASIC_API_URL}/${USERS}`, usersRouter);
-// app.use(`${BASIC_API_URL}/${AUTH}`, authRouter);
-// app.use(`${BASIC_API_URL}/${DIABETES}`, diabetesRouter);
-// app.use(`${BASIC_API_URL}/${CONTENTS}`, contentsRouter);
-// app.use(`${BASIC_API_URL}/${COMMENT}`, commentRouter);
-// app.use(`${BASIC_API_URL}/${LIKE}`, likeRouter);
-// app.use(`${BASIC_API_URL}/${SEARCH}`, searchRouter);
-// app.use(`${BASIC_API_URL}/image`, imageRouter);
-// // app.use(`${INDEX_PATH}`, (_, res) => res.send("연결 완료"));
+import path from "path";
+import { BASIC_API_URL } from "./constants/path";
 
 class App {
   app: Application;
-  constructor(controllers: Controller) {
+  constructor(controllers: Controller[]) {
     this.app = express();
+    this.app.set("port", process.env.PORT || 5000);
+
     this.connectDB();
     this.initializeMeddlewares();
-
+    this.initalControllers(controllers);
+    this.initalStaticRoutes();
+    this.initializeErrorHandler();
   }
   private connectDB() {
     if (process.env.NODE_ENV !== "production") {
@@ -69,7 +38,7 @@ class App {
     mongoose.connection.on("disconnected", this.connectDB);
   }
 
-  private initializeMeddlewares() {
+  private initializeMeddlewares(): void {
     this.app.use(morgan("dev"));
     this.app.use(
       cors({
@@ -86,15 +55,44 @@ class App {
     this.app.use(express.json());
     this.app.use(cookieParser(process.env.COOKIE_SECRET));
   }
-  private initializeErrorHandler() {
+
+  private initalStaticRoutes(): void {
+    // 아래 이미지 url은 스토리지 사용하게 되면 initalStaticRoutes이 필요없음
+    this.app.use(
+      "/img/uimg",
+      express.static(path.join(__dirname, "..", "/src/", "uploads/userImg"))
+    );
+    this.app.use(
+      "/img/pimg",
+      express.static(path.join(__dirname, "..", "/src/", "uploads/postImg"))
+      );
+  }
+
+  private initalControllers(controllers: Controller[]): void {
+    this.app.use("/api/page", (req: Request, res: Response) => {
+      res.send("page page");
+    });
+    controllers.forEach((controller) => {
+      this.app.use(`${BASIC_API_URL}`, controller.router);
+    });
+    this.app.use("/api/*", (req, res) =>
+      res.send("해당 페이지를 찾을 수 없습니다.")
+    );
+    this.app.use("/*", (req, res) =>
+      res.send("당클린 서버에 연결 성공")
+    );
+  }
+
+  private initializeErrorHandler(): void {
     this.app.use(errorMiddleware);
   }
 
-  public listen() {}
+  public listen(): void {
+    const port = this.app.get("port");
+    this.app.listen(port, () => {
+      console.log(`Server Listening on http://localhost:${port}`);
+    });
+  }
 }
-
-// app.listen(PORT, () =>
-//   console.log(`Server Listening on http://localhost:${PORT}`)
-// );
 
 export default App;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import "module-alias/register";
 import validateEnv from "./config/validEnv";
+import App from "./app";
+import ContentsController from "./apis/contents/contents.controller";
+import ContentsService from "./apis/contents/contents.service";
 
 validateEnv();
 
 /* services */
+const contentsService = new ContentsService()
 
-const server = new App([new BoardController(boardService)]);
+const server = new App([new ContentsController(contentsService)]);
 
 server.listen();
